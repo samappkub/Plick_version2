d830264a5fff76621733ffba38b895f2c96d397a
Samuel Appiah-Kubi <samappkub@gmail.com>
2017-10-31 22:54:02 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "users");
+
+// Define URL
+define("ROOT_PATH", "/plick.dev/");
+define("ROOT_URL", "http://localhost/plick.dev/");
